759c8b8f0fbdb2bbb049666f3b400af45948a41f
leonerescoder <leonardoneres93@gmail.com>
2026-02-23 22:22:31 -0300

    aula_23_02

--- projetomeo/AnaliseMeteorologica.java
+++ projetomeo/AnaliseMeteorologica.java
@@ -14,7 +14,7 @@ public class AnaliseMeteorologica {
         }
         
         
-        calculo = (max * 0.7) + (min * 0.3);
+         calculo = (max * 0.7) + (min * 0.3);
 
 
 
@@ -25,10 +25,48 @@ public class AnaliseMeteorologica {
     }    
 
 
+
+    public static String classificaClima( double tempMedia,  int umidadeMedia) {
+       
+      
+        if(tempMedia > 30 && umidadeMedia > 75){
+            return("MUITO QUENTE E ÚMIDO");
+        }else if(tempMedia >=20 && tempMedia<= 25){
+            return("CONFORTAVEL");
+
+        }else{
+            return("");
+        }
+
+
+
+
+        
+    }
+
+
     public static void main(String[] args) {
+        // Temperaturas: [máxima, mínima] para 5 cidades
+        double[][] temperaturas = {
+             {32.5, 22.1},  // Cidade 1
+             {28.3, 18.7},  // Cidade 2
+             {35.8, 24.9},  // Cidade 3
+             {30.2, 20.5},  // Cidade 4
+             {25.7, 15.3}   // Cidade 5
+            };
+
+        // Umidades: [manhã, tarde, noite] para 5 cidades
+        int[][] umidades = {
+            {85, 60, 75},  // Cidade 1
+            {78, 55, 70},  // Cidade 2
+            {90, 65, 80},  // Cidade 3
+            {82, 58, 72},  // Cidade 4
+            {75, 50, 68}   // Cidade 5
+            };
+        
+
         
         
-    
 
 
        
