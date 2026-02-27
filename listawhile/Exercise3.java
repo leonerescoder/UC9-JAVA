8831c126fe8c287d22262b62b288d6b53ae09cf3
leonerescoder <leonardoneres93@gmail.com>
2026-02-27 20:31:51 -0300

    leo

---
+++ listawhile/Exercise3.java
@@ -0,0 +1,17 @@
+package listawhile;
+
+public class Exercise3 {
+    public static void main(String[] args) {
+        for(int i = 2;i <=50;i++){
+
+            if(i % 2 == 0){
+                System.out.println(i);
+
+            }else {
+                System.out.println("Número não é par");
+            }
+           
+
+       };
+    }
+}
