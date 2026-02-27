58ccfeb7eafef6353cf31d0a6365a96f6af4cd66
leonerescoder <leonardoneres93@gmail.com>
2026-02-27 20:18:35 -0300

    aula27_02

--- listawhile/Exercise2.java
+++ listawhile/Exercise2.java
@@ -2,11 +2,10 @@ package listawhile;
 
 public class Exercise2 {
     public static void main(String[] args) {
-       int numero;
-       numero=1;
+      
 
-       for(int i = 20;i > 20;i++){
-            System.out.println(numero);
+       for(int i = 1;i <=20;i++){
+            System.out.println(i);
 
        };
     }
