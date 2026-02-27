785ed3398b3cacc5d096ab8c9eecb00d179aac55
leonerescoder <leonardoneres93@gmail.com>
2026-02-27 20:13:51 -0300

    E
    aula_2702

---
+++ listawhile/Exercise2.java
@@ -0,0 +1,13 @@
+package listawhile;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+       int numero;
+       numero=1;
+
+       for(int i = 20;i > 20;i++){
+            System.out.println(numero);
+
+       };
+    }
+}
--- projetomeo/AnaliseMeteorologica.java
+++ projetomeo/AnaliseMeteorologica.java
@@ -13,28 +13,35 @@ public class AnaliseMeteorologica {
 
 
     public static String classificaClima( double tempMedia,  int umidadeMedia){
+
        
       
         if(tempMedia > 30 && umidadeMedia > 75){
             return("MUITO QUENTE E ÚMIDO");
-        }else if(tempMedia >=20 && tempMedia<= 25){
+        }else if(tempMedia >=20 && tempMedia <=25 || umidadeMedia >=50 && umidadeMedia <=70 ){
             return("CONFORTAVEL");
 
         }else if(tempMedia < 15 && umidadeMedia < 50){
             return("FRIO E SECO");
+
         }else{
             return("");
         }
-
+    }
+        
 
 
 
         
-    }
-
+    
+            
+            
 
     public static void main(String[] args) {
         
+        double temperature ;
+        int umidade;
+        Double qtd = new Double(umidade);
         
         
         double[][] temperaturas = {
@@ -43,19 +50,31 @@ public class AnaliseMeteorologica {
              {35.8, 24.9},  // Cidade 3
              {30.2, 20.5},  // Cidade 4
              {25.7, 15.3}   // Cidade 5
-            };
+        };
             
             
-            int[][] umidades = {
+        int[][] umidades = {
                 {85, 60, 75},  // Cidade 1
                 {78, 55, 70},  // Cidade 2
                 {90, 65, 80},  // Cidade 3
                 {82, 58, 72},  // Cidade 4
                 {75, 50, 68}   // Cidade 5
-            };
-            
-             //System.out.println(calcularMediaPonderadaTemperatura(temperaturas[0][0], temperaturas[0][1]));
+        };
+        
+        temperature =calcularMediaPonderadaTemperatura(temperaturas[0][0],temperaturas[0][1]);
+        umidade = calcularMediaPonderadaTemperatura(umidades[0][0],umidades[0][1]);
 
+        System.out.println(calcularMediaPonderadaTemperatura(temperature, umidade));
+        
+    
+
+
+
+    
+
+
+     // System.out.println(calcularMediaPonderadaTemperatura(temperaturas[0][0], temperaturas[0][1])); Teste da Função
+    
 
              
 
