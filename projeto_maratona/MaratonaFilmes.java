4595c2b3a1b1aea0ca2b3e36572e02956762fb50
leonerescoder <leonardoneres93@gmail.com>
2026-02-27 21:13:00 -0300

    move paste

---
+++ projeto_maratona/MaratonaFilmes.java
@@ -0,0 +1,105 @@
+package projeto_maratona;
+
+import java.util.Scanner;
+
+
+public class MaratonaFilmes {
+    public static void ExibirCabecalho (){
+        System.out.printf("============================================= \n");
+        System.out.printf("\tGERENCIADOR DE MARATONA DE FILMES  \n" );
+        System.out.printf("=============================================");
+
+    }
+    public static double calcularTempoTotal(int filmes, double duracaoMedia) {
+       double calculotemp;
+       Double qtdFilmes = new Double(filmes);
+       calculotemp = qtdFilmes * duracaoMedia;
+
+
+       // System.out.printf("Cálculos: \nTempo total: "+ calculotemp + " horas");
+        return calculotemp;
+        
+    }
+    public static String classificarMaratona (double horas){
+        
+        
+
+        
+       
+
+        if(horas <= 4){
+            
+            return("Maratona leve");
+
+        } else if(horas > 4 && horas <= 8)  {
+             
+            return("Maratona Moderada");
+            
+        }else{
+              
+            return("Maratona Epica");
+        }
+
+        
+
+
+        
+       
+
+
+    }
+
+     public static boolean isValida(int filmes){
+        return filmes > 0;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        
+        String user;
+
+        
+        int filmes;
+        double horas;
+        int calculo;
+        double duracaoMedia;
+        double duracaoTotal;
+        boolean trueornote;
+
+
+
+        
+        //Recebendo as valores na variavel
+        filmes=10;
+        duracaoMedia=2.0;
+        user="LEO";
+
+        ExibirCabecalho();
+        System.out.println( );
+        System.out.println( "Informações da Maratona:  ");
+        System.out.println( "Usuario: "+ user);
+        System.out.println( "Filmes planejados: "+ filmes);
+        System.out.println( "Duração média por filme: " + duracaoMedia);
+        duracaoTotal = calcularTempoTotal(filmes, duracaoMedia);
+        duracaoTotal = calcularTempoTotal(filmes, duracaoMedia);
+        //classificarMaratona(duracaoTotal);
+        //System.out.println("Retorno do Total "+ duracaoTotal); teste
+        System.out.printf("Cálculos: \nTempo total: "+ duracaoTotal + " horas");
+        System.out.println();
+        System.out.println("Classificação: "+ classificarMaratona(duracaoTotal));
+        trueornote = isValida(filmes);
+        System.out.println("Validação: Planejamento válido? " + trueornote);
+
+        
+        
+
+
+
+        
+        
+    }
+}
